19519ba7eb800d32492af6ff3e84be69edd459ad
Simone Girardi <Simone.Girardi@Marconi.loc>
2024-02-26 11:24:46 +0100

    sus

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DatiService } from '../service/dati.service';
+import { Libro } from '../domain/Libro';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  constructor(public datiservice : DatiService){}
+  
+  form : FormGroup
+  libro : Libro
+
+  ngOnInit(){
+    this.form = new FormGroup({
+      id : new FormControl("",[Validators.required]),
+    })
+  }
+
+  cercaLibro() {
+    this.datiservice.getLibro(this.form.get("id").value).subscribe(data => {
+      this.libro = data
+    })
+  }
 
 }
